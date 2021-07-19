5ddf931edf96e3b425d128400919f6174e5ba769
hammad30820 <mohammed.18is072@cambridge.edu.in>
2021-07-19 20:36:44 +0530

    init

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <link rel="stylesheet" type="text/css" href="css/navbar.css">
+      <a class="navbar-brand" href="index.php" style="color:DodgerBlue;">SPARKS BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color:DodgerBlue;">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color:SlateBlue;">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color:DodgerBlue;">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color:SlateBlue;">Transaction History</a>
+              </li>
+      </div>
+      </nav>
